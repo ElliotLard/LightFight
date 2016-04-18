3f90cd12d902ca248e657d48df5540bdbb4c1575
ElliotLard <el1458@cs.ship.edu>
2016-04-17 20:05:38 -0400

    first commit

---
+++ LightFight Test/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	// How fast the player will move when they move.
+	public float speed;
+
+	// Gives us access to the RB2D, so we can move the player.
+	private Rigidbody2D rb2d;
+
+	public GameObject shot;
+	public Transform firePoint;
+	public float fireRate;
+	private float nextFire;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButton(0) && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, firePoint.position, firePoint.rotation);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		// Move with physics
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+
+		// Rotate to Face the Mouse
+		Vector3 pos = Camera.main.WorldToScreenPoint (transform.position);
+		Vector3 dir = Input.mousePosition - pos;
+		float angle = Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg - 90;
+		transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+		}
+	}
+}
---
+++ LightFight Test/Assets/Scripts/ShotBehavior.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotBehavior : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () 
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.velocity = transform.up * speed;
+	}
+
+	// Deletes upon hitting wall
+	void OnCollisionEnter2D(Collision2D otherObj) {
+		if (otherObj.gameObject.tag == "Border")
+			Destroy (gameObject);
+
+		if (otherObj.gameObject.tag == "Mirror")
+        {
+            /* r = i−2(i·n)n
+             * Source: http://stanford.io/1zco8xk
+             * where n is the vector perpendicular to the mirror,
+             * r is the vector exiting the mirror,
+             * and i is the vector hitting the mirror
+             */
+            Vector3 enterVelocity = rb2d.velocity; // n
+            Vector3 exitVelocity = new Vector3(0,0,0); // r
+            Quaternion rotation = Quaternion.Euler(exitVelocity);
+            Destroy(gameObject);
+            ContactPoint2D contactPoint = otherObj.contacts[0];
+            Instantiate(gameObject, contactPoint.point, gameObject.transform.rotation);
+            /*
+             * As of right now, they are colliding constantly with the mirror
+             * once they hit it. That's bad. However that doens't explain why they
+             * don't move even if they're sent to the origin.
+             */
+        }
+	}
+
+
+}
